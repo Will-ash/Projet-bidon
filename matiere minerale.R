178fdfea2fea9df286c5b30ac67f7b1dc56232c4
William Lefranc <willefranc35@gmail.com>
2025-01-22 17:04:02 +0100

    ajout de lignes

--- matiere minerale.R
+++ matiere minerale.R
@@ -1,1 +1,6 @@
-Cailloux genoux hiboux chouchoux poupoux troutroux
+## Cailloux genoux hiboux chouchoux poupoux troutroux ##
+
+#On va aller a la montagne
+library(usethise)
+#library(usethis)
+#usethis::use_git()  # utiliser pour créer le repository dans le dossier du projet R
